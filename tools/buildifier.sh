7885c76e1ba6287c20e7db66650c986cf95f1734
Jeremy Elbourn <jelbourn@google.com>
2018-07-25 13:17:20 -0700

    Capture skylark files in buildifier script

--- tools/buildifier.sh
+++ tools/buildifier.sh
@@ -5,4 +5,4 @@
 # to avoid errors, since this project's bazelrc has this set the other way.
 bazel build --noincompatible_disallow_old_style_args_add --noshow_progress @com_github_bazelbuild_buildtools//buildifier
 
-find . -name "BUILD" -or -name "BUILD.bazel" | xargs $(bazel info bazel-bin)/external/com_github_bazelbuild_buildtools/buildifier/*/buildifier
+find . -name "BUILD" -or -name "BUILD.bazel" -or -iname "*.bzl" | xargs $(bazel info bazel-bin)/external/com_github_bazelbuild_buildtools/buildifier/*/buildifier
